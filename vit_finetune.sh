223617addf9d22b026243ac6563510a8473701de
yjh0410 <1394571815@qq.com>
2023-08-24 08:13:03 +0800

    finetune ViT-Tiny on CIFAR10

--- vit_finetune.sh
+++ vit_finetune.sh
@@ -2,13 +2,13 @@
 python mae_finetune.py \
         --cuda \
         --dataset cifar10 \
-        -m vit_nano \
+        -m vit_tiny \
         --batch_size 256 \
         --img_size 32 \
         --patch_size 2 \
         --max_epoch 50 \
         --wp_epoch 5 \
-        --mae_pretrained weights/cifar10/mae_vit_nano/mae_vit_nano_cifar10.pth
+        --mae_pretrained weights/cifar10/mae_vit_tiny/mae_vit_tiny_cifar10.pth
 
 # --------------- Finetune on ImageNet ---------------
 # python mae_finetune.py \
