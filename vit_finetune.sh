da68c9d53fa6fa788ea8050f49e6e41ceeedf516
yjh0410 <1394571815@qq.com>
2023-10-26 09:56:42 +0800

    pretrain mae-vit-tiny on imagenet-1k

--- vit_finetune.sh
+++ vit_finetune.sh
@@ -16,10 +16,12 @@ python mae_finetune.py \
 #         --cuda \
 #         --root path/to/imagenet_1k \
 #         --dataset imagenet_1k \
-#         -m vit_nano \
+#         -m vit_tiny \
 #         --batch_size 256 \
 #         --img_size 224 \
 #         --patch_size 16 \
-#         --max_epoch 50 \
+#         --max_epoch 100 \
 #         --wp_epoch 5 \
-#         --mae_pretrained weights/imagenet_1k/mae_vit_nano/mae_vit_nano_imagenet_1k.pth
+#         --base_lr 0.0005 \
+#         --weight_decay 0.05 \
+#         --mae_pretrained weights/imagenet_1k/mae_vit_tiny/mae_vit_tiny_imagenet_1k.pth
--- vit_pretrain.sh
+++ vit_pretrain.sh
@@ -1,24 +1,28 @@
 # --------------- Pretrain on CIFAR ---------------
-python mae_pretrain.py \
-        --cuda \
-        --dataset cifar10 \
-        -m mae_vit_tiny \
-        --batch_size 256 \
-        --img_size 32 \
-        --patch_size 2 \
-        --max_epoch 400 \
-        --wp_epoch 40 \
-        --eval_epoch 20 \
+# python mae_pretrain.py \
+#         --cuda \
+#         --dataset cifar10 \
+#         -m mae_vit_tiny \
+#         --batch_size 256 \
+#         --img_size 32 \
+#         --patch_size 2 \
+#         --max_epoch 400 \
+#         --wp_epoch 40 \
+#         --eval_epoch 20 \
 
 
 # --------------- Pretrain on ImageNet ---------------
-# python mae_pretrain.py \
-#         --cuda \
-#         --root path/to/imagenet_1k \
-#         --dataset imagenet_1k \
-#         -m mae_vit_nano \
-#         --batch_size 256 \
-#         --img_size 224 \
-#         --patch_size 16 \
-#         --max_epoch 400 \
-#         --wp_epoch 40
+python mae_pretrain.py \
+        --cuda \
+        --root /data/datasets/imagenet-1k \
+        --dataset imagenet_1k \
+        -m mae_vit_tiny \
+        --batch_size 256 \
+        --img_size 224 \
+        --patch_size 16 \
+        --max_epoch 800 \
+        --wp_epoch 40 \
+        --optimizer adamw \
+        --base_lr 0.00015 \
+        --weight_decay 0.05 \
+        --mask_ratio 0.75 \
