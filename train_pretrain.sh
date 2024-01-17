fe3e793b9907d1a4cc183ee037eb9781383bbb35
yjh0410 <1394571815@qq.com>
2024-01-17 18:00:34 +0800

    update

--- train_pretrain.sh
+++ train_pretrain.sh
@@ -1,16 +1,15 @@
-# ------------------- Model setting -------------------
-MODEL=$1 #"mae_resnet18"
-
+# ------------------- Args setting -------------------
+MODEL=$1
+BATCH_SIZE=$2
+DATASET=$3
+DATASET_ROOT=$4
+WORLD_SIZE=$5
 
 # ------------------- Training setting -------------------
-## Batch size
-BATCH_SIZE=$2 #256
-
 ## Epoch
 MAX_EPOCH=800
 WP_EPOCH=40
 EVAL_EPOCH=20
-
 if [[ $MODEL == *"mae_vit"* ]]; then
     MASK_RATIO=0.75
     # Optimizer config
@@ -42,32 +41,19 @@ fi
 
 
 # ------------------- Dataset setting -------------------
-DATASET="imagenet_1k"
 if [[ $DATASET == "cifar10" ]]; then
-    # Data root
-    ROOT=$3 #"none"
-    # Image config
     IMG_SIZE=32
     PATCH_SIZE=2
     NUM_CLASSES=10
 elif [[ $DATASET == "cifar100" ]]; then
-    # Data root
-    ROOT=$3 #"none"
-    # Image config
     IMG_SIZE=32
     PATCH_SIZE=2
     NUM_CLASSES=100
 elif [[ $DATASET == "imagenet_1k" || $DATASET == "imagenet_22k" ]]; then
-    # Data root
-    ROOT=$3 #"/data/datasets/imagenet-1k/"
-    # Image config
     IMG_SIZE=224
     PATCH_SIZE=16
     NUM_CLASSES=1000
 elif [[ $DATASET == "custom" ]]; then
-    # Data root
-    ROOT=$3 #"/Users/liuhaoran/Desktop/python_work/classification/dataset/Animals/"
-    # Image config
     IMG_SIZE=224
     PATCH_SIZE=16
     NUM_CLASSES=2
@@ -78,11 +64,10 @@ fi
 
 
 # ------------------- Training pipeline -------------------
-WORLD_SIZE=$4
 if [ $WORLD_SIZE == 1 ]; then
     python main_pretrain.py \
             --cuda \
-            --root ${ROOT} \
+            --root ${DATASET_ROOT} \
             --dataset ${DATASET} \
             --color_format ${COLOR_FORMAT} \
             -m ${MODEL} \
@@ -98,10 +83,10 @@ if [ $WORLD_SIZE == 1 ]; then
             --weight_decay ${WEIGHT_DECAY} \
             --mask_ratio ${MASK_RATIO}
 elif [[ $WORLD_SIZE -gt 1 && $WORLD_SIZE -le 8 ]]; then
-    python -m torch.distributed.run --nproc_per_node=${WORLD_SIZE} --master_port 1668 main_pretrain.py \
+    python -m torch.distributed.run --nproc_per_node=${WORLD_SIZE} --master_port 1634 main_pretrain.py \
             --cuda \
             -dist \
-            --root ${ROOT} \
+            --root ${DATASET_ROOT} \
             --dataset ${DATASET} \
             --color_format ${COLOR_FORMAT} \
             -m ${MODEL} \
